cb8fea3256c735ddd35041ca293b34a8a0e2c4d6
hiba <hibaabdelkarim2000@gmail.com>
2021-03-30 23:46:19 +0300

    feat : added register page and signin form component

---
+++ src/Screen/Registration/Registration.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Register from "../../Components/Register/Register"
+import "./Registration.css"
+export default function Registration() {
+    return (
+        <div className = "registration-page">
+            <Register/>
+        </div>
+    )
+}
